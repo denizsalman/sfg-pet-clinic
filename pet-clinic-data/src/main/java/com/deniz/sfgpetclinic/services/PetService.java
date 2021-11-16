cafe24f14e6bceaf88052334ce6fdabd07657f69
denizsalman <ddenizsalman@gmail.com>
2021-11-16 21:41:12 +0300

    refactoring to crud interface. Closes #11

--- pet-clinic-data/src/main/java/com/deniz/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/deniz/sfgpetclinic/services/PetService.java
@@ -1,15 +1,7 @@
 package com.deniz.sfgpetclinic.services;
 
-import com.deniz.sfgpetclinic.model.Owner;
 import com.deniz.sfgpetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long>{
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
