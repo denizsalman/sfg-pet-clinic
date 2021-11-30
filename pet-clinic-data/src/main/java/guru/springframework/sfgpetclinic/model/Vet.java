3f492f011fd6511ddbbc1218e6225ef6ea6f1bc4
denizsalman <ddenizsalman@gmail.com>
2021-11-30 23:05:46 +0300

    added specialities, added to Vet. Closes #28

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,9 +1,20 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * @author deniz
  * 04/11/2021
  */
 public class Vet extends Person {
-	
+
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
