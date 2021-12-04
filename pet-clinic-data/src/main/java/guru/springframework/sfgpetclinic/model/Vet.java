d7e17f7a9c5db1dff4b5142ee47cb5455f27b556
denizsalman <ddenizsalman@gmail.com>
2021-12-04 18:37:13 +0300

    updated vet to use JPA. Closes #38

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,5 +1,6 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -7,8 +8,14 @@ import java.util.Set;
  * @author deniz
  * 04/11/2021
  */
+@Entity
+@Table(name = "vet")
 public class Vet extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_speciality",
+            joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
